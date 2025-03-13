9b183af896fcca89124f8b0f10624517f15aa2ad
Filipe Pereira <filipe.simoes.pereira2817@gmail.com>
2025-03-13 10:31:12 +0000

    Test writer with other options in Rust

--- Rust/examples/writer_example.rs
+++ Rust/examples/writer_example.rs
@@ -1,5 +1,18 @@
-use opendal::{services, Operator, Result};
+use opendal::{services, Operator, Result, Buffer};
 use opendal::layers::LoggingLayer;
+
+async fn read_with_logging(op: &Operator, path: &str) -> Result<Buffer> {
+    match op.read(path).await {
+        Ok(res) => {
+            println!("Read from path ({}): {}", path, String::from_utf8_lossy(&res.to_vec()));
+            Ok(res)
+        }
+        Err(e) => {
+            println!("Failed to read from path ({}): {:?}", path, e);
+            Err(e)
+        }
+    }
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 
@@ -16,27 +29,29 @@ async fn main() -> Result<()> {
         .to_vec();
 
     // Write
-    let mut w = op.writer_with(path).append(true).await?;
+    let mut w = op.writer_with(path).await?;
     w.write(data.clone()).await?;
-    w.write(data.clone()).await?;
+
+    // Read before closing
+    read_with_logging(&op, path).await?;
+
     w.close().await?;
     
-    // Read
-    match op.read(path).await {
-        Ok(data) => {
-            println!("Read from path ({}): {}", path, String::from_utf8_lossy(&data.to_vec()));
+    // Read after closing
+    read_with_logging(&op, path).await?;
 
-        }
-        Err(e) => {
-            println!("Failed to read from path ({}): {:?}", path, e);
-            return Err(e)
-        }
-    }
-
-    // Write again
-    let mut w = op.writer_with(path).append(true).await?;
-    w.write("Another write".as_bytes().to_vec()).await?;
+    // Write again with multiple options
+    let mut w = op
+        .writer_with(path)
+        .append(true)
+        .chunk(128)
+        .concurrent(2)
+        .await?;
+    w.write(data[..data.len() / 2].to_vec()).await?;
+    w.write(data[data.len() / 2..].to_vec()).await?;
     w.close().await?;
+
+    read_with_logging(&op, path).await?;
 
     Ok(())
 }
